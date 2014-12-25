a7b4dabed56301a3bdc8dcc0404b80472847b28c
urokomori <urokomori@gmail.com>
2014-12-25 22:43:08 +0900

    [Improve]The right clock is now redrawn every minute.

--- script/main.js
+++ script/main.js
@@ -1,32 +1,123 @@
-function showClock() {
-   var nowTime = new Date();
-   var nowHour = nowTime.getHours();
-   var nowMinute = nowTime.getMinutes();
-   var nowSecond = nowTime.getSeconds();
-   var nowTimeText = ("0"+nowHour).slice(-2) + ":" + ("0"+nowMinute).slice(-2) + ":" + ("0"+nowSecond).slice(-2);
-   document.getElementById("clock").innerHTML = nowTimeText;
+setInterval('checkClock()',1000);
+setInterval('drawClockHour()',60000);
+
+function onload(){
+  checkClock();
+  drawHourCircle();
+}
+
+function checkClock() {
+   var now = new Date();
+  drawDigitalClock(now);
+
+  if(now.getHours()==0 && now.getMinutes()==0 && now.getSeconds()==0){
+    clearHourCircle();
   }
+}
 
-setInterval('showClock()',1000);
+function drawDigitalClock(now){
+   var nowTimeText = ("0"+now.getHours()).slice(-2)
+                       + ":" + ("0"+now.getMinutes()).slice(-2)
+                       + ":" + ("0"+now.getSeconds()).slice(-2);
 
-function draw(){
+   document.getElementById("clock").innerHTML = nowTimeText;
+}
+
+function clearHourCircle() {
   var canvas = document.getElementById('canvas');
+
   if (canvas.getContext){
     var context = canvas.getContext('2d');
   }
 
-  var centerX = 320;
-  var centerY = 300;
+  canvas.clearRect(0, 0, 600, 600);
+}
 
-  context.strokeStyle = '#262626';
+function drawHourCircle(){
+  var canvas = document.getElementById('canvas');
+
+  if (canvas.getContext){
+    var context = canvas.getContext('2d');
+  }
+
+  var centerPosition = {
+    x:320,
+    y:300
+  }
+  var radius = 200;
+  var colorset = getColorSet();
+
+  drawClockEdge(centerPosition, radius, colorset.edge, context);
+  drawClockNumber(centerPosition, radius, colorset.number, context);
+  drawClockFilling(centerPosition, radius, colorset.filling, context);
+  drawPartingLine(centerPosition, radius, colorset.partingLine, context);
+}
+
+function drawClockEdge(center, radius, color, context){
+  context.strokeStyle = color;
   context.beginPath();
-  context.arc(centerX, centerY, 200, 0, Math.PI*2, false);
+  context.arc(center.x, center.y, radius, 0, Math.PI*2, false);
+  context.stroke();
+}
+
+function drawClockNumber(center, radius, color, context){
+  context.font = "36px 'Wire One'";
+  context.strokeText("0", center.x-10, center.y-radius-12);
+  context.strokeText("3", center.x+radius/2+50, center.y-radius/2-50);
+  context.strokeText("6", center.x+radius+18, center.y+12);
+  context.strokeText("9", center.x+radius/2+55, center.y+radius/2+70);
+  context.strokeText("12", center.x-18, center.y+radius+36);
+  context.strokeText("15", center.x-radius/2-80, center.y+radius/2+75);
+  context.strokeText("18", center.x-radius-50, center.y+12);
+  context.strokeText("20", center.x-radius/2-90, center.y-radius/2-50);
+}
+
+function drawClockFilling(center, radius, color, context){
+  var now = new Date();
+  var nowPosition = (Math.PI*2) * ((now.getHours() * 60 + now.getMinutes()) / (60 * 24));
+
+  context.strokeStyle = color;
+  context.beginPath();
+  context.moveTo(center.x, center.y);
+  context.arc(center.x, center.y, Math.floor(radius*0.95), -Math.PI/2, -Math.PI/2 + nowPosition , false);
+  context.closePath();
+  context.fillStyle = color;
+  context.fill();  
+}
+
+function drawPartingLine(center, radius, color, context){
+  context.strokeStyle = color;
+
+  context.beginPath();
+  context.moveTo(center.x-radius, center.y);
+  context.lineTo(center.x+radius, center.y);
   context.stroke();
 
-  context.font = "36px 'Wire One'";
-  context.strokeText("0", centerX-10, centerY-200-18);
-  context.strokeText("6", centerX+200+18, centerY);
-  context.strokeText("12", centerX-18, centerY+200+36);
-  context.strokeText("18", centerX-200-50, centerY);
+  context.beginPath();
+  context.moveTo(center.x, center.y-radius);
+  context.lineTo(center.x, center.y+radius);
+  context.stroke();  
 
+  var distance = Math.floor(radius/Math.sqrt(2));
+
+  context.beginPath();
+  context.moveTo(center.x-distance , center.y-distance );
+  context.lineTo(center.x+distance , center.y+distance );
+  context.stroke();
+
+  context.beginPath();
+  context.moveTo(center.x+distance , center.y-distance );
+  context.lineTo(center.x-distance , center.y+distance );
+  context.stroke();
+}
+
+function getColorSet(){
+  var colorset = {
+    edge: '#262626',
+    number: '#262626',
+    filling: '#FF6860',
+    partingLine: '#CCDFCC'
+  }  
+
+  return colorset;
 }
